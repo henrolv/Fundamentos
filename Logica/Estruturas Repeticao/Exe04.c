c0569df4dae88031b4d5a8cba330957818fcca65
Eduardo Henrique <henriquexazs@gmail.com>
2025-01-26 15:39:15 -0300

    Exercício 05

---
+++ Logica/Estruturas Repeticao/Exe04.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+	float nota1 = 0.0, nota2 = 0.0, mediaNotas = 0.0;
+
+	printf("Digite a primeira nota: ");
+	scanf("%f",&nota1);
+
+	while(nota1 < 0 || nota1 > 10){
+		printf(".......\nErro, as notas devem estar no intervalo (0 - 10)\n");
+		printf(".......\nDigite a primeira nota\n");
+		scanf("%f",&nota1);
+	}
+
+	printf("Digite a segunda nota: ");
+	scanf("%f",&nota2);
+
+	while(nota2 < 0 || nota2 > 10){
+		printf(".......\nErro, as notas devem estar no intervalo (0 - 10)\n");
+		printf(".......\nDigite a segunda nota\n");
+		scanf("%f",&nota2);
+	}
+
+	mediaNotas = (nota1 + nota2) / 2;
+
+	printf("Media Notas = %.2f\n",mediaNotas);
+
+return 0;
+
+}
