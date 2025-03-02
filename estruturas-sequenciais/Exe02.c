44b3e31760c6c8fa56eff423e62999e6390550c1
Eduardo Henrique <henrmcz@gmail.com>
2025-03-02 14:31:27 -0300

    Exercício 02

---
+++ estruturas-sequenciais/Exe02.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+	int num1 = 0, num2 = 0, soma = 0;
+
+	printf("Digite o primeiro numero:");
+	scanf("%d",&num1);
+	printf("Digite o segundo numero:");
+	scanf("%d",&num2);
+
+	soma = num1 + num2;
+
+	printf("Soma = %d\n",soma);
+
+return 0;
+
+}
+
